d1e439fe280d2b7e57682e0b31a0837e9b5fcb81
Tamojit Saha <sahatamo1995@gmail.com>
2019-06-12 16:56:53 +0530

    Update NIRS_parser.py

--- NIRS_parser.py
+++ NIRS_parser.py
@@ -2,8 +2,7 @@
 """
 Created on Wed Jun 05 17:15:17 2019
 
-@author: Tamojit Saha
-@website: tamojitsaha.info
+@author: Tamojit
 """
 import numpy as np
 from time import sleep
@@ -62,53 +61,92 @@ def readSerial():
         print (e)
 
 
-bigdata = []
-def parse(str1,timeData):    
+bigData = []
+initial_buffer = []    
+firstSerialDataFlag = False
+
+def parse(str1,timeData ):    
     buffer = [] 
-    initial_buffer = []    
+    global initial_buffer
+    global bigData
+    global firstSerialDataFlag
 
     y,m,d,h,m,s,us = timeData
+    timeStampData = str(h)+":"+str(m)+":"+str(s)
+
     # print (y,m,d,h,m,s)
+    str1 = ''.join(str1)
     str1 = str1.rstrip()
-    
     for e in str1.split('\t') : 
         buffer.append(int(e))
 
     if len(initial_buffer) < 1:
-        for i in buffer: initial_buffer.append(i)
+        firstSerialDataFlag = True
+
+        for i in buffer: 
+            initial_buffer.append(i)
+        
+        if firstSerialDataFlag == True:
+            firstSerialDataList = []
+            timeStampData += ":"+str('0')
+            firstSerialDataList.append(timeStampData)
+            firstSerialDataList.extend(buffer[2:])
+            bigData.append(firstSerialDataList)
+            # print (firstSerialDataList)
+            firstSerialDataFlag = False
+
+        # print (initial_buffer)
         del buffer[:]
 
     else:
-        # str2 mean read serial data
-        timeData = str(h)+":"+str(m)+":"+str(s)
-        str2 = str2.rstrip()
+        # str1 mean read serial data        
+        # str1 = str1.rstrip()
         #list -> timestamp and all data
         #this list will be appended in a different 
         #list named bigData()
         format_data_list = []
         # add serial data to buffer
-        for e in str2.split('\t') : 
-            buffer.append(int(e))
+        # for e in str1.split('\t') : 
+        #     buffer.append(int(e))
         # compare if the current serial data is new
         if buffer[0] > initial_buffer[0]:
+
             bufferLength = len(buffer)
             newMicros = initial_buffer[1] + buffer[1]
-            buffer[1] = newMicros
             
-            format_data_list.append(timeData)
+            # buffer[1] = newMicros
+
+            # print (buffer[1])
+            
+            
+            format_data_list.append(timeStampData+":"+str(newMicros))
             format_data_list.extend(buffer[2:]) 
-            bigData.append(format_data_list)            
+            bigData.append(format_data_list)
+            initial_buffer[1] =  newMicros
+                       
 
 
 
 
 
 if __name__== '__main__':
-    str1 = "0\t0\t375\t362\t359\t"
-    str2 = "1\t20014\t785\t457\t654"
+    str1 = [["0\t0\t375\t362\t359"],
+     ["1\t20014\t785\t457\t654"],
+     ["2\t20024\t378\t362\t359"],
+     ["3\t20034\t785\t457\t654"],
+     ["4\t20044\t375\t362\t359"],
+     ["5\t20054\t785\t457\t654"]]
+    # str1[6] = "6\t20064\t375\t362\t359"
+    # str1[7] = "7\t20074\t785\t457\t654"
+    # str1[8] = "8\t20084\t375\t362\t359"
+    # str1[9] = "9\t20094\t785\t457\t654"
+   
+    for i in range(len(str1)):
+        parse(str1[i], timestamp())          
     # readSerial()
-    parse(str1, timestamp())
+    #parse(str1, timestamp())
     timestamp()
+    print (bigData) 
     
 
 
